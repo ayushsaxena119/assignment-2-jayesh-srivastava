54542aba5cf6e196b277bc2e37915d6ed05f4762
jayesh-srivastava <jayesh0200@gmail.com>
2020-03-31 16:34:38 +0530

    Committing main class of question 2.

--- src/problem2/main/MyMain.java
+++ src/problem2/main/MyMain.java
@@ -5,7 +5,45 @@
  *  Time: 7:15 PM
  */
 package problem2.main;
+
+import problem1.mybst.MyBinarySearchTree;
+import problem1.node.TreeNode;
+
 // executable class
 // use problem1.mybst.MyBinarySearchTree class for creating binary tree
 public class MyMain {
+    public static void preOrder(TreeNode root){
+        if(root == null)    return;
+        else{
+            System.out.print(root.getData()+"\t");
+            preOrder(root.getLeft());
+            preOrder(root.getRight());
+
+        }
+    }
+    public static void postOrder(TreeNode root){
+        if(root == null)    return;
+        else{
+            postOrder(root.getLeft());
+            postOrder(root.getRight());
+            System.out.print(root.getData()+"\t");
+        }
+    }
+    public static void prepost(){
+        MyBinarySearchTree obj = new MyBinarySearchTree();
+        postOrder(obj.getRoot());
+        System.out.println();
+        preOrder(obj.getRoot());
+    }
+    public static void main(String[] args) {
+        MyBinarySearchTree obj = new MyBinarySearchTree();
+        obj.insert(62);
+        obj.insert(55);
+        obj.insert(59);
+        obj.insert(2);
+        obj.insert(25);
+        obj.insert(99);
+        obj.insert(36);
+        prepost();
+    }
 }
