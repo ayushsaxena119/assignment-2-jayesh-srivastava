531aafecd810f05f2e8df5b37101e81629749960
jayesh-srivastava <jayesh0200@gmail.com>
2020-03-31 18:38:57 +0530

    Committing node class of question 4.

---
+++ src/problem4/myqueue/Node.java
@@ -0,0 +1,26 @@
+package problem4.myqueue;
+public class Node {
+    int data;
+    Node next;
+
+    public Node(int data) {
+        this.data = data;
+        next=null;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+
+    public int getData() {
+        return data;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+}
