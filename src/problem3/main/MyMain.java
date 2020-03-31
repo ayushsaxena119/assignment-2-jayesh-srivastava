25cb9ac713194c3f3d066e905ae11753c2d3a80e
jayesh-srivastava <jayesh0200@gmail.com>
2020-03-31 17:17:16 +0530

    Committing main class of question 3.

--- src/problem3/main/MyMain.java
+++ src/problem3/main/MyMain.java
@@ -7,5 +7,41 @@
 package problem3.main;
 // executable class
 // use problem5.student.Student class to create object of student
+import problem3.myqueue.MyPriorityQueue;
+import problem3.node.Node;
+
+import java.util.*;
 public class MyMain {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        MyPriorityQueue obj=new MyPriorityQueue();
+        System.out.print("To Insert press 1, To Dequeue press 2, to traverse press3, to print number of students press 4:- ");
+        int counter = sc.nextInt();
+        while (counter!=0){
+            switch( counter ){
+                case 1:
+                    System.out.print("Enter Roll you want to insert:- ");
+                    obj.enqueue(sc.nextInt());
+                    break;
+                case 2:
+                    Node result=obj.dequeue();
+                    if(result!=null) {
+                        System.out.println("The roll is " + result.getRoll() + ". Your roll is also deleted from Queue");
+                    }else{
+                        System.out.println("The Queue is empty");
+                    }
+                    break;
+                case 3:
+                    obj.traversing();
+                    break;
+                case 4:
+                    System.out.println("Number of students "+obj.getSize());
+                    break;
+                default:
+                    System.out.println("Invalid Option! Enter again.");
+            }
+            System.out.print("To Insert press 1, To Dequeue press 2, To traverse press3, To print number of students press 4, To exit press 0:- ");
+            counter=sc.nextInt();
+        }
+    }
 }
