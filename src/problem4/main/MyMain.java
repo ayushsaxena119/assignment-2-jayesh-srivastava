847dac93ea0ca9d4a5fcaef70efb6d21086a2bcc
jayesh-srivastava <jayesh0200@gmail.com>
2020-03-31 18:52:17 +0530

    Committing main class of question 4.

--- src/problem4/main/MyMain.java
+++ src/problem4/main/MyMain.java
@@ -7,5 +7,25 @@
 package problem4.main;
 // executable class
 // use problem1.mybst.MyBinarySearchTree class for creating binary tree
+
+import problem4.myqueue.MyQueue;
+
+import static problem4.myqueue.MyQueue.*;
+import problem1.mybst.MyBinarySearchTree;
+import problem4.myqueue.MyQueue;
 public class MyMain {
+    public static void main(String[] args) {
+        MyBinarySearchTree obj = new MyBinarySearchTree();
+        obj.insert(10);
+        obj.insert(3);
+        obj.insert(30);
+        obj.insert(37);
+        obj.insert(13);
+        obj.insert(1);
+        obj.insert(7);
+
+        MyQueue mq = new MyQueue();
+        mq.preSuccessor(obj.getRoot());
+        mq.getPreSucc();
+    }
 }
