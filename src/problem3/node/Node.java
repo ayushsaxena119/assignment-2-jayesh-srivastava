33ef0a9a9f5bcdd6e111e8d23ef489d3d2a1bac7
jayesh-srivastava <jayesh0200@gmail.com>
2020-03-31 16:55:42 +0530

    Committing node class of question 3.

--- src/problem3/node/Node.java
+++ src/problem3/node/Node.java
@@ -7,4 +7,27 @@
 package problem3.node;
 
 public class Node {
+    private int roll;
+    private  Node next;
+
+    public Node(int roll){
+        this.roll=roll;
+        next=null;
+    }
+
+    public int getRoll() {
+        return roll;
+    }
+
+    public void setRoll(int roll) {
+        this.roll = roll;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
 }
